03b8b64d4e938e0fbb808ac5b8e9babe111ea2b5
Ethan Gruenemeier <EGruenemeier984@west-mec.org>
2019-03-04 13:39:04 -0700

    Final Commit
    Finished debugging the createBar function and got the percentages to function properly also finished adding all comments. This project is ready for review.

--- vw_results.js
+++ vw_results.js
@@ -23,46 +23,64 @@
    
       
 */
-
+// This var is set equal to a heading tag that is set to the raceTitle var
 var reportHTML = `<h1>${raceTitle}</h1>`;
-
+// This for loop loops through the race length array starting at 0.
 for (var i = 0; i < race.length; i++) {
+    // sets the totalVotes var to 0
     var totalVotes = 0;
-    race.forEach(calcSum);
+    // This runs the calcSum function on every value in the votes array
+    votes[i].forEach(calcSum);
+    // This adds the table to the reportHTML var along with the candidateRows function with two arguments.
     reportHTML += `<table> <caption>${race[i]}</caption> <tr><th>Candidate</th><th>Votes</th></tr>`;
     reportHTML += candidateRows(i, totalVotes);
     reportHTML += "</table>"
 }
-
+// This sets the reportHTML var equal to the first tag with the name section inner HTML
 document.getElementsByTagName("section")[0].innerHTML = reportHTML;
-
+// This function takes in the parameters of raceNum and totalVotes 
 function candidateRows(raceNum, totalVotes) {
+    // this var of rowHTML is set to an empty string.
     var rowHTML = "";
+    // This for loop starts at 0 and stops at 2 while iterating by 1.
     for (var j = 0; j <= 2; j++) {
+        // This var is set to the multidimensional array called candidate with two index values one being raceNum and the other is j.
         var candidateName = candidate[raceNum][j];
+        // This var is set to the multidimensional array called party with two index values one being raceNum and the other is j.
         var candidateParty = party[raceNum][j];
+        // This var is set to the multidimensional array called votes with two index values one being raceNum and the other is j.
         var candidateVotes = votes[raceNum][j];
+        // this var is set to the calcPercent function with the arguments of candidateVotes and totalVotes.
         var candidatePercent = calcPercent(candidateVotes, totalVotes);
+        // This adds the table row to the rowHTML var
+        rowHTML += `<tr> <td>${candidateName} (${candidateParty})</td> <td>${candidateVotes.toLocaleString()} (${candidatePercent.toFixed(1)})</td>`
+        // This for loop starts at 0 and stops at the value of candidatePercent while iterating by 1
         for (var k = 0; k < candidatePercent; k++) {
-            createBar(candidateParty, candidatePercent);
+            // This adds the createBar function to the rowHTML creating the bar graphs 
+            rowHTML += createBar(candidateParty, candidatePercent);
         }
-        rowHTML += `<tr> <td>${candidateName} (${candidateParty})</td> <td>${candidateVotes.toLocaleString()} (${candidatePercent.toFixed(1)})</td></tr>`
+        // closes the table row.
+        rowHTML += `</tr>`;
     }
+    // returns this as the output of the function.
     return rowHTML;
 }
-
+// This function creates the bars with a parameter of partyType 
 function createBar(partyType) {
+    // This var is set to an empty string 
     var barHTML = "";
+    // This is a switch/case conditional statement that checks the party type of each race.
     switch (partyType) {
-        case D:
+        case "D":
             barHTML += "<td class='dem'></td>";
             break;
-        case R:
+        case "R":
             barHTML += "<td class='rep'></td>";
             break;
-        case I:
+        case "I":
             barHTML += "<td class='ind'></td>";
     }
+    // returns this as the output
     return barHTML;
 }
 
